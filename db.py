360b21472fdbfe4df439e214ad2f1c4f8f93bf78
Olli Hyvärinen <ollielias44@gmail.com>
2026-09-14 11:47:32 +0300

    Added db execute and query functions to db.py

--- db.py
+++ db.py
@@ -1,7 +1,25 @@
 import sqlite3
+from flask import g
 
 def get_connection():
     con = sqlite3.connect("database.db")
     con.row_factory = sqlite3.Row
     con.execute("PRAGMA foreign_keys=ON")
     return con
+
+
+def execute(sql, params=[]):
+    con = get_connection()
+    result = con.execute(sql, params)
+    con.commit()
+    g.last_insert_id = result.lastrowid
+    con.close()
+
+def last_insert_id():
+    return g.last_insert_id
+
+def query(sql, params=[]):
+    con = get_connection()
+    result = con.execute(sql, params).fetchall()
+    con.close()
+    return result
