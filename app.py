176946bd844d94224db8799bacf3d6e5032dc963
Olli Hyvärinen <ollielias44@gmail.com>
2026-09-14 12:47:42 +0300

    add registration fuctionality

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+import sqlite3
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template, request
+from werkzeug.security import generate_password_hash
+import db
 
 app = Flask(__name__)
 
@@ -11,3 +14,15 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_hash =generate_password_hash(password)
+    try:
+        sql = "INSERT INTO Users (username, created_at, password_hash) VALUES (?, datetime('now'), ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "username already taken"
+    return "Tunnus luotu"
